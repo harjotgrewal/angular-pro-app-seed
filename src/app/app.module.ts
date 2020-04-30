7f7b8c5b5afa0898898dff690ba4f597595c81b1
harjotgrewal <harjot@unmetric.com>
2020-04-30 11:07:42 -0400

    [WIP] Fitness app - base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,3 +30,17 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+
+/*
+var firebaseConfig = {
+  apiKey: "AIzaSyDnAppj6Msld9ZfV430zxIK4SaU63gVjf4",
+  authDomain: "fitness-app-440f7.firebaseapp.com",
+  databaseURL: "https://fitness-app-440f7.firebaseio.com",
+  projectId: "fitness-app-440f7",
+  storageBucket: "fitness-app-440f7.appspot.com",
+  messagingSenderId: "96542219201",
+  appId: "1:96542219201:web:0cb974255323e16bb2ed40",
+  measurementId: "G-KCZBXGN7WJ"
+};
+*/
